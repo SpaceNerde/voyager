c0370e0f64b8a99fdf9782e3f13ce8cc2e44fa52
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-01-02 20:02:03 +0100

    add quit commad and create input field

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod ui;
 
 use std::{fs, io};
-use crossterm::event::{EnableMouseCapture};
-use crossterm::execute;
-use crossterm::terminal::{enable_raw_mode, EnterAlternateScreen};
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture, KeyCode, KeyEventKind};
+use crossterm::event::Event::Key;
+use crossterm::{event, execute};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
 use tui::backend::CrosstermBackend;
 use tui::Terminal;
 
@@ -21,10 +22,39 @@ fn main() {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend).unwrap();
 
+    let mut input_text:Vec<char> = Vec::new();
+
     loop {
         terminal.draw(|f| {
-            ui::main_layout(f, "c");
+            ui::main_layout(f, "c", &input_text);
         }).unwrap();
+
+        if let Key(key) = event::read().unwrap() {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Esc => {
+                        break;
+                    }
+                    KeyCode::Backspace => {
+                        input_text.pop();
+                    }
+                    KeyCode::Char(c) => {
+                        input_text.push(c);
+                    }
+                    _ => {}
+                }
+            }
+        }
     }
+    disable_raw_mode().unwrap();
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    ).unwrap();
+    terminal.show_cursor().unwrap();
+
+    terminal.clear().unwrap();
     // load_all_files("C:\\");
 }
+
--- src/ui.rs
+++ src/ui.rs
@@ -4,7 +4,7 @@ use tui::layout::{Constraint, Direction, Layout};
 use tui::text::Spans;
 use tui::widgets::{Block, Borders, Paragraph};
 
-pub fn main_layout<B: Backend>(f: &mut Frame<B>, path: &str) {
+pub fn main_layout<B: Backend>(f: &mut Frame<B>, path: &str, input_text: &Vec<char>) {
     // define areas
     let chunks = Layout::default()
         .direction(Direction::Vertical)
@@ -19,21 +19,22 @@ pub fn main_layout<B: Backend>(f: &mut Frame<B>, path: &str) {
 
     // config widgets
     // Block to display what folder you are in
-    let block_1 = Block::default()
+    let block = Block::default()
         .title("Current Folder")
         .borders(Borders::ALL);
-    let paragraph = Paragraph::new(Spans::from(path)).block(block_1);
+    let paragraph = Paragraph::new(Spans::from(path)).block(block);
     f.render_widget(paragraph, chunks[0]);
 
     // Block to display content inside folder
-    let block_2 = Block::default()
+    let block = Block::default()
         .title("Content")
         .borders(Borders::ALL);
-    f.render_widget(block_2, chunks[1]);
+    f.render_widget(block, chunks[1]);
 
-    // Block for controlling environment
-    let block_3 = Block::default()
+    // Input field
+    let block = Block::default()
         .title("Command Line")
         .borders(Borders::ALL);
-    f.render_widget(block_3, chunks[2]);
+    let paragraph = Paragraph::new(input_text.iter().cloned().collect::<String>()).block(block);
+    f.render_widget(paragraph, chunks[2]);
 }
